4fda23a7a447dfbba6a616caa27666accbc3ffe2
Wanomir <ioan.romadin@gmail.com>
2024-08-30 13:19:59 +0300

    feat: upload Taskfile

--- proxy/internal/app/app.go
+++ proxy/internal/app/app.go
@@ -23,6 +23,48 @@ func init() {
 	prometheus.MustRegister(appInfo)
 }
 
+//package config
+//
+//import (
+//"github.com/ilyakaznacheev/cleanenv"
+//)
+//
+//type (
+//	Config struct {
+//		GRPC
+//		Log
+//		PG
+//	}
+//
+//	GRPC struct {
+//		Port int `env-required:"true" env:"GRPC_PORT"`
+//	}
+//
+//	Log struct {
+//		Level string `env-required:"true" env:"LOG_LEVEL"`
+//	}
+//
+//	PG struct {
+//		Host          string `env-required:"true" env:"PG_HOST"`
+//		User          string `env-required:"true" env:"PG_USER"`
+//		Password      string `env-required:"true" env:"PG_PASSWORD"`
+//		UserAdmin     string `env-required:"true" env:"PG_USER_ADMIN"`
+//		PasswordAdmin string `env-required:"true" env:"PG_PASSWORD_ADMIN"`
+//		Database      string `env-required:"true" env:"PG_DATABASE"`
+//	}
+//)
+//
+//func NewConfig() (*Config, error) {
+//	cfg := &Config{}
+//
+//	err := cleanenv.ReadEnv(cfg)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	return cfg, nil
+//}
+
 type Config struct {
 	host       string
 	port       string
